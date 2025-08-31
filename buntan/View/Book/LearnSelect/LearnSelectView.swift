102644393ecc8a2efb7036d5699f4c62b7c64f5e
nyoki610 <yuya.nyoki@gmail.com>
2025-08-31 14:19:08 +0900

    fix: function call order in onAppear action

--- buntan/View/Book/LearnSelect/LearnSelectView.swift
+++ buntan/View/Book/LearnSelect/LearnSelectView.swift
@@ -89,8 +89,8 @@ struct LearnSelectView: View {
             .background(CustomColor.background)
             .navigationBarBackButtonHidden(true)
             .onAppear {
-                adjustSelectedRange()
                 viewModel.onAppearAction(userInput: userInput)
+                adjustSelectedRange()
             }
     }
     
