8b6ecb85ff12f8671b82cd169141f68a3a5b2662
nyoki610 <yuya.nyoki@gmail.com>
2025-09-11 15:23:58 +0900

    delete: unnecessary properties of AppConfig

--- buntan/Network/AppConfig.swift
+++ buntan/Network/AppConfig.swift
@@ -4,8 +4,6 @@ protocol AppConfigProtocol {
     
     var apiKey: String? { get }
     var baseURL: String? { get }
-    var getLatestCardsPath: String? { get }
-    var getLatestVersionPath: String? { get }
 }
 
 struct AppConfig: AppConfigProtocol {
@@ -14,21 +12,15 @@ struct AppConfig: AppConfigProtocol {
     
     let apiKey: String?
     let baseURL: String?
-    let getLatestCardsPath: String?
-    let getLatestVersionPath: String?
     
     private enum Key: String {
         case API_KEY
         case API_BASE_URL
-        case LATEST_CARDS_PATH
-        case LATEST_VERSION_PATH
     }
     
     private init() {
         self.apiKey = Self.readEnvironmentVariable(named: Key.API_KEY.rawValue)
         self.baseURL = Self.readEnvironmentVariable(named: Key.API_BASE_URL.rawValue)?.replacingOccurrences(of: "\\", with: "")
-        self.getLatestCardsPath = Self.readEnvironmentVariable(named: Key.LATEST_CARDS_PATH.rawValue)?.replacingOccurrences(of: "\\", with: "")
-        self.getLatestVersionPath = Self.readEnvironmentVariable(named: Key.LATEST_VERSION_PATH.rawValue)?.replacingOccurrences(of: "\\", with: "")
     }
     
     private static func readEnvironmentVariable(named name: String) -> String? {
