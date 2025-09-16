c9092ad1f0f7cac49b7fbded5ee30377cef55b37
nyoki610 <yuya.nyoki@gmail.com>
2025-09-16 22:19:51 +0900

    update: fetch version number from Bundle

--- buntan/View/Main/LogoView.swift
+++ buntan/View/Main/LogoView.swift
@@ -44,9 +44,11 @@ struct LogoView: View {
             
             Spacer()
             
-            Text("ver. 1.1.1")
-                .font(.system(size: responsiveScaled(20, 1.5)))
-                .fontWeight(.semibold)
+            if let versionNumber = viewModel.versionNumber {
+                Text("ver. \(versionNumber)")
+                    .font(.system(size: responsiveScaled(20, 1.5)))
+                    .fontWeight(.semibold)
+            }
             
             Spacer()
             Spacer()
