6a7040e082dfa8b9785b831ab28ae45f071ec650
nyoki610 <yuya.nyoki@gmail.com>
2025-09-30 09:16:20 +0900

    update: replace SheetRealmAPI.updateCardsStatus() with CardUseCase.updateStatus()

--- buntan/Realm/API/SheetRealmAPI/updateCardsStatusAPI.swift
+++ buntan/Realm/API/SheetRealmAPI/updateCardsStatusAPI.swift
@@ -1,29 +1,29 @@
-import Foundation
-
-
-extension SheetRealmAPI {
-    
-    static func updateCardsStatus(
-        viewModel: BaseLearnViewViewModel,
-        eikenGrade: EikenGrade,
-        bookCategory: BookCategory
-    ) -> Bool {
-        
-        var newStatusTupleList: [(String, Int)] = []
-        
-        for (index, card) in viewModel.cards.enumerated() {
-            
-            if viewModel.leftCardsIndexList.contains(index) {
-                newStatusTupleList.append((card.id, Card.CardStatus.learning.rawValue))
-            }
-            else if viewModel.rightCardsIndexList.contains(index) {
-                newStatusTupleList.append((card.id, Card.CardStatus.completed.rawValue))
-            }
-        }
-        
-        return SheetRealmCruds.updateCardsStatus(
-            newStatusTupleList: newStatusTupleList,
-            bookCategory: bookCategory
-        )
-    }
-}
+//import Foundation
+//
+//
+//extension SheetRealmAPI {
+//    
+//    static func updateCardsStatus(
+//        viewModel: BaseLearnViewViewModel,
+//        eikenGrade: EikenGrade,
+//        bookCategory: BookCategory
+//    ) -> Bool {
+//        
+//        var newStatusTupleList: [(String, Int)] = []
+//        
+//        for (index, card) in viewModel.cards.enumerated() {
+//            
+//            if viewModel.leftCardsIndexList.contains(index) {
+//                newStatusTupleList.append((card.id, Card.CardStatus.learning.rawValue))
+//            }
+//            else if viewModel.rightCardsIndexList.contains(index) {
+//                newStatusTupleList.append((card.id, Card.CardStatus.completed.rawValue))
+//            }
+//        }
+//        
+//        return SheetRealmCruds.updateCardsStatus(
+//            newStatusTupleList: newStatusTupleList,
+//            bookCategory: bookCategory
+//        )
+//    }
+//}
