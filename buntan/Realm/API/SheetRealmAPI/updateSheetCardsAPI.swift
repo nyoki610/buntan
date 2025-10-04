12fba434525c93d0c92fffd2c76fe4260ad8b054
nyoki610 <yuya.nyoki@gmail.com>
2025-10-04 16:33:07 +0900

    add: SheetService.syncSheetCards

--- buntan/Realm/API/SheetRealmAPI/updateSheetCardsAPI.swift
+++ buntan/Realm/API/SheetRealmAPI/updateSheetCardsAPI.swift
@@ -1,88 +1,88 @@
-import Foundation
-
-
-extension SheetRealmAPI {
-    
-    static func updateSheetCards(
-        grade: EikenGrade,
-        newCards: [Card]
-    ) -> Bool {
-        
-        guard confirmTargetSheetExsits(grade: grade) else { return false }
-        
-        let sheetRepository = SheetRepository()
-        guard let sheet: Sheet = try? sheetRepository.getSheet(for: grade) else { return false }
-        
-        let oldCards: [Card] = sheet.cardList
-        let updatedNewCards: [Card] = getUpdatedNewCards(newCards: newCards, oldCards: oldCards)
-                
-        do {
-            try sheetRepository.updateCardList(of: sheet, with: updatedNewCards)
-        } catch {
-            return false
-        }
-        
-        guard deleteUnnecessaryObjects(oldCards: oldCards) else { return false }
-        
-        return true
-    }
-    
-    private static func confirmTargetSheetExsits(grade: EikenGrade) -> Bool {
-        
-        let repository = RealmRepository()
-        guard let allSheets: [Sheet] = try? repository.fetchAll() else { return false }
-        let grades: Set<EikenGrade> = Set(allSheets.map { $0.grade })
-        
-        if !grades.contains(grade) {
-            do {
-                let newSheet = SheetFactory.createNew(from: grade)
-                try repository.insert(newSheet)
-            } catch {
-                return false
-            }
-        }
-        
-        return true
-    }
-    
-    
-    private static func getUpdatedNewCards(
-        newCards: [Card],
-        oldCards: [Card]
-    ) -> [Card] {
-        
-        let oldCardsDict: [String: (Card.CardStatus, Card.CardStatus)] = Dictionary(
-            uniqueKeysWithValues: oldCards.map { card in
-                (card.word, (card.statusFreq, card.statusPos))
-            }
-        )
-        let updatedNewCards = newCards.map { card in
-            var newCard = card
-            if let (oldStatusFreq, oldStatusPos) = oldCardsDict[newCard.word] {
-                newCard.statusFreq = oldStatusFreq
-                newCard.statusPos = oldStatusPos
-            }
-            return newCard
-        }
-        
-        return updatedNewCards
-    }
-    
-    
-    private static func deleteUnnecessaryObjects(oldCards: [Card]) -> Bool {
-        
-        let unnecessaryCardIds = Set(oldCards.map { $0.id })
-        let unnecessaryInfoIds = Set(oldCards.flatMap { $0.infoList.map { $0.id }})
-        
-        let repository = RealmRepository()
-        
-        do {
-            try repository.deleteObjects(by: unnecessaryCardIds, of: RealmCard.self)
-            try repository.deleteObjects(by: unnecessaryInfoIds, of: RealmInfo.self)
-        } catch {
-            return false
-        }
-        
-        return true
-    }
-}
+//import Foundation
+//
+//
+//extension SheetRealmAPI {
+//    
+//    static func updateSheetCards(
+//        grade: EikenGrade,
+//        newCards: [Card]
+//    ) -> Bool {
+//        
+//        guard confirmTargetSheetExsits(grade: grade) else { return false }
+//        
+//        let sheetRepository = SheetRepository()
+//        guard let sheet: Sheet = try? sheetRepository.getSheet(for: grade) else { return false }
+//        
+//        let oldCards: [Card] = sheet.cardList
+//        let updatedNewCards: [Card] = getUpdatedNewCards(newCards: newCards, oldCards: oldCards)
+//                
+//        do {
+//            try sheetRepository.updateCardList(of: sheet, with: updatedNewCards)
+//        } catch {
+//            return false
+//        }
+//        
+//        guard deleteUnnecessaryObjects(oldCards: oldCards) else { return false }
+//        
+//        return true
+//    }
+//    
+//    private static func confirmTargetSheetExsits(grade: EikenGrade) -> Bool {
+//        
+//        let repository = RealmRepository()
+//        guard let allSheets: [Sheet] = try? repository.fetchAll() else { return false }
+//        let grades: Set<EikenGrade> = Set(allSheets.map { $0.grade })
+//        
+//        if !grades.contains(grade) {
+//            do {
+//                let newSheet = SheetFactory.createNew(from: grade)
+//                try repository.insert(newSheet)
+//            } catch {
+//                return false
+//            }
+//        }
+//        
+//        return true
+//    }
+//    
+//    
+//    private static func getUpdatedNewCards(
+//        newCards: [Card],
+//        oldCards: [Card]
+//    ) -> [Card] {
+//        
+//        let oldCardsDict: [String: (Card.CardStatus, Card.CardStatus)] = Dictionary(
+//            uniqueKeysWithValues: oldCards.map { card in
+//                (card.word, (card.statusFreq, card.statusPos))
+//            }
+//        )
+//        let updatedNewCards = newCards.map { card in
+//            var newCard = card
+//            if let (oldStatusFreq, oldStatusPos) = oldCardsDict[newCard.word] {
+//                newCard.statusFreq = oldStatusFreq
+//                newCard.statusPos = oldStatusPos
+//            }
+//            return newCard
+//        }
+//        
+//        return updatedNewCards
+//    }
+//    
+//    
+//    private static func deleteUnnecessaryObjects(oldCards: [Card]) -> Bool {
+//        
+//        let unnecessaryCardIds = Set(oldCards.map { $0.id })
+//        let unnecessaryInfoIds = Set(oldCards.flatMap { $0.infoList.map { $0.id }})
+//        
+//        let repository = RealmRepository()
+//        
+//        do {
+//            try repository.deleteObjects(by: unnecessaryCardIds, of: RealmCard.self)
+//            try repository.deleteObjects(by: unnecessaryInfoIds, of: RealmInfo.self)
+//        } catch {
+//            return false
+//        }
+//        
+//        return true
+//    }
+//}
