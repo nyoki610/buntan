b3349ec9a563930276afe7b74febe9f41dff1e1f
nyoki610 <yuya.nyoki@gmail.com>
2025-10-04 19:52:15 +0900

    update: LogoViewViewModel with Dependencies

--- buntan/Dependency/UseCase/CheckForcedUpdateUseCase.swift
+++ buntan/Dependency/UseCase/CheckForcedUpdateUseCase.swift
@@ -7,7 +7,22 @@
 
 import Foundation
 
-struct CheckForcedUpdateUseCase {
+protocol CheckForcedUpdateUseCaseProtocol {
+    func isForcedUpdateRequired() async -> CheckForcedUpdateUseCase.RequiredUpdateType?
+}
+
+struct CheckForcedUpdateUseCase: CheckForcedUpdateUseCaseProtocol {
+
+    struct Dependency {
+        let remoteConfigRepository: any RemoteConfigRepositoryProtocol = RemoteConfigRepository.shared
+        let infoPlistRepository: any InfoPlistRepositoryProtocol = InfoPlistRepository()
+    }
+
+    private let dependency: Dependency
+    
+    init(dependency: Dependency = .init()) {
+        self.dependency = dependency
+    }
     
     enum RequiredUpdateType {
         case force
@@ -15,9 +30,9 @@ struct CheckForcedUpdateUseCase {
         case notRequired
     }
     
-    static func isForcedUpdateRequired() async -> RequiredUpdateType? {
+    func isForcedUpdateRequired() async -> RequiredUpdateType? {
         
-        guard let requiredAppVersionId = try? await RemoteConfigRepository.shared.string(.requiredAppVersionId) else {
+        guard let requiredAppVersionId = try? await dependency.remoteConfigRepository.string(.requiredAppVersionId) else {
             return nil
         }
         
@@ -25,15 +40,15 @@ struct CheckForcedUpdateUseCase {
             return .force
         }
 
-        guard let recommendedAppVersionId = try? await RemoteConfigRepository.shared.string(.recommendedAppVersionId) else {
+        guard let recommendedAppVersionId = try? await dependency.remoteConfigRepository.string(.recommendedAppVersionId) else {
             return nil
         }
 
         return isUpdateRequired(for: recommendedAppVersionId) ? .softForce : .notRequired
     }
     
-    private static func isUpdateRequired(for id: String) -> Bool {
-        guard let appVersionId = InfoPlistRepository.value(for: .appVersionId) else {
+    private func isUpdateRequired(for id: String) -> Bool {
+        guard let appVersionId = dependency.infoPlistRepository.value(for: .appVersionId) else {
             return false
         }
         return appVersionId.compare(id, options: .numeric) == .orderedAscending
