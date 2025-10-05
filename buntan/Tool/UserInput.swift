3b72af809a95dfb7e61e7f281400ba42e26a2325
nyoki610 <yuya.nyoki@gmail.com>
2025-10-05 15:19:13 +0900

    add: require method to BookUserInput

--- buntan/Tool/UserInput.swift
+++ buntan/Tool/UserInput.swift
@@ -16,6 +16,22 @@ final class BookUserInput: UserInput {
     @Published var todaysWordCount: Int?
 }
 
+extension BookUserInput {
+    enum Error: Swift.Error {
+        case valueIsNil(keyPath: String)
+    }
+    
+    func require<Value>(
+        _ keyPath: KeyPath<BookUserInput, Value?>,
+        keyPathName: String = #function
+    ) throws -> Value {
+        guard let value = self[keyPath: keyPath] else {
+            throw Error.valueIsNil(keyPath: keyPathName)
+        }
+        return value
+    }
+}
+
 final class CheckUserInput: UserInput {
     @Published var selectedGrade: EikenGrade = .first
 }
