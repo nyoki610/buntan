b4af6e4d8c5400bc122f383455ad029c61675d3f
nyoki610 <yuya.nyoki@gmail.com>
2025-10-25 13:50:27 +0900

    add: FlickWordCardView and FlickSentenceCardView

--- buntan/Tool/CutomText.swift
+++ buntan/Tool/CutomText.swift
@@ -2,8 +2,38 @@ import Foundation
 
 enum CustomText {
     
+    // TODO: replace boldSentence(card: Card, ...) with boldSentence(card: LearnCard, ...)
     /// card.sentence の card.startPosition から card.endPosition までを太字にする関数
     static func boldSentence(card: Card, size: CGFloat, isUnderlined: Bool) -> AttributedString {
+        
+        let sentence = card.sentence
+        let startPosition = card.startPosition
+        let endPosition = card.endPosition
+        
+        var attributedString = AttributedString(sentence)
+        
+        guard startPosition < endPosition, startPosition >= 0, endPosition <= sentence.count else {
+            return attributedString
+        }
+
+        let startIndex = sentence.index(sentence.startIndex, offsetBy: startPosition)
+        let endIndex = sentence.index(sentence.startIndex, offsetBy: endPosition)
+        
+        if let range = attributedString.range(of: Substring(sentence[startIndex...endIndex])) {
+            /// Text(AttributedString) に .font(.system(size: )) を適用しても, AttributedString,font が優先される
+            var attributes = AttributeContainer()
+            attributes.font = .system(size: size, weight: .bold)
+            attributedString[range].setAttributes(attributes)
+            if isUnderlined {
+                attributedString[range].underlineStyle = .single
+            }
+            
+        } else { return AttributedString("") }
+        
+        return attributedString
+    }
+    
+    static func boldSentence(card: LearnCard, size: CGFloat, isUnderlined: Bool) -> AttributedString {
         
         let sentence = card.sentence
         let startPosition = card.startPosition
